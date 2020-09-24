22a7aec7a61b8f77cfdb2a3cbe1bedeb01789244
grisov <grisov.gmail@mailnull.com>
2020-09-24 13:32:44 +0300

    Added Software Constructor script to automatically build the add-on

---
+++ buildVars.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+    # add-on Name, internal for nvda
+    "addon_name" : "quickDictionary",
+    # Add-on summary, usually the user visible name of the addon.
+    # Translators: Summary for this add-on to be shown on installation and add-on information.
+    "addon_summary" : _("Quick Dictionary"),
+    # Add-on description
+    # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+    "addon_description" : _("""Bilingual Dictionary add-on for quick translation selected words or phrases."""),
+    # version
+    "addon_version" : "1.0",
+    # Author(s)
+    "addon_author" : u"Oleksandr Gryshchenko <grisov.nvaccess@mailnull.com>",
+    # URL for the add-on documentation support
+    "addon_url" : "https://github.com/grisov/quickDictionary",
+    # Documentation file name
+    "addon_docFileName" : "readme.html",
+    # Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
+    "addon_minimumNVDAVersion": "2019.3",
+    # Last NVDA version supported/tested (e.g. "2020.3.0", ideally more recent than minimum version)
+    "addon_lastTestedNVDAVersion": "2020.3",
+    # Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
+    "addon_updateChannel" : None,
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = [
+    'addon/globalPlugins/*/*.py',
+]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
