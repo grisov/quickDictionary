6bc0e5364a98f6f52c02ca095ea2e12ec69edc07
grisov <grisov.gmail@mailnull.com>
2020-09-26 17:00:41 +0300

    Updated Add-on Scons Template

--- buildVars.py
+++ buildVars.py
@@ -3,42 +3,48 @@
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
+
 # Full getext (please don't change)
-_ = lambda x : x
+_ = lambda x: x  # NOQA: E731 # Never, EVER change this line
+
 
 # Add-on information variables
 addon_info = {
-    # add-on Name, internal for nvda
-    "addon_name" : "quickDictionary",
-    # Add-on summary, usually the user visible name of the addon.
-    # Translators: Summary for this add-on to be shown on installation and add-on information.
-    "addon_summary" : _("Quick Dictionary"),
-    # Add-on description
-    # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-    "addon_description" : _("""Online Dictionary add-on for quick translation selected words or phrases."""),
-    # version
-    "addon_version" : "1.0",
-    # Author(s)
-    "addon_author" : u"Oleksandr Gryshchenko <grisov.nvaccess@mailnull.com>",
-    # URL for the add-on documentation support
-    "addon_url" : "https://github.com/grisov/quickDictionary",
-    # Documentation file name
-    "addon_docFileName" : "readme.html",
-    # Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
-    "addon_minimumNVDAVersion": "2019.3",
-    # Last NVDA version supported/tested (e.g. "2020.3.0", ideally more recent than minimum version)
-    "addon_lastTestedNVDAVersion": "2020.3",
-    # Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
-    "addon_updateChannel" : None,
+	# add-on Name/identifier, internal for NVDA
+	"addon_name": "quickDictionary",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on
+	# to be shown on installation and add-on information found in Add-ons Manager.
+	"addon_summary": _("Quick Dictionary"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description": _("""Online Dictionary add-on for quick translation selected words or phrases."""),
+	# version
+	"addon_version": "1.0",
+	# Author(s)
+	"addon_author": "Oleksandr Gryshchenko <grisov.nvaccess@mailnull.com>",
+	# URL for the add-on documentation support
+	"addon_url": "https://github.com/grisov/quickDictionary",
+	# Documentation file name
+	"addon_docFileName": "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	"addon_minimumNVDAVersion": "2019.3",
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion": "2020.3",
+	# Add-on update channel (default is None, denoting stable releases,
+	# and for development releases, use "dev".)
+	# Do not change unless you know what you are doing!
+	"addon_updateChannel": None,
 }
 
 
-import os.path
+import os.path  # NOQA
+
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
 pythonSources = [
-    'addon/globalPlugins/*/*.py',
+	"addon/globalPlugins/*/*.py"
 ]
 
 # Files that contain strings for translation. Usually your python sources
