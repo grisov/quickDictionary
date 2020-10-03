4933c81cd5f4984b3eb3de82d1419e2dd9cf3e6d
grisov <grisov.gmail@mailnull.com>
2020-10-03 20:01:52 +0300

    Represents the list of profiles of speech synthesizers

--- addon/globalPlugins/quickDictionary/synthesizers.py
+++ addon/globalPlugins/quickDictionary/synthesizers.py
@@ -49,3 +49,56 @@ class Profile(object):
 	def lang(self, lang: str):
 		self._lang = lang
 		return self
+
+
+class Profiles(object):
+
+	def __init__(self):
+		self._path = os.path.join(config.getUserDefaultConfigPath(), "%s.pickle" % _addonName)
+		self._profs = {}
+		self._default = Profile().update()
+		self.load()
+
+	def load(self):
+		if not os.path.exists(self._path): return
+		with open(self._path, 'rb') as f:
+			data = pickle.load(f)
+		if 'version' not in data:
+			data = {'version': 0}
+		self._profs = dict((key, Profile(val['name'], val['conf'], val['lang'])) if isinstance(key,int) else (key,val) for key,val in data.items())
+
+	def save(self):
+		profs = {}
+		for slot in sorted(self._profs, key=lambda slot: str(slot)):
+			if isinstance(slot, int):
+				profs[slot] = {
+					'name': self._profs[slot].name,
+					'conf': self._profs[slot].conf,
+					'lang': self._profs[slot].lang
+				}
+			else:
+				profs[slot] = self._profs[slot]
+		with open(self._path, 'wb') as f:
+			pickle.dump(profs, f)
+
+	def __getitem__(self, id):
+		if id not in self._profs:
+			self._profs[id] = Profile()
+		return self._profs[id]
+
+	def __iter__(self):
+		for slot in sorted(self._profs, key=lambda s: str(s)):
+			if isinstance(slot, int):
+				yield slot, self._profs[slot]
+
+	def remove(self, id: int):
+		try:
+			self._profs.pop(id)
+		except KeyError:
+			pass
+
+	def currentAsDefault(self):
+		self._default = Profile().update()
+
+	def restoreDefault(self):
+		self._default.set()
