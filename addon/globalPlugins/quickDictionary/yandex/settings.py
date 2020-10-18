f79ff95c388b41a914d992ffdf3e7735ef30ae34
grisov <grisov.gmail@mailnull.com>
2020-10-18 13:23:20 +0300

    Settings panel elements are divided into general and service-specific

--- addon/globalPlugins/quickDictionary/yandex/settings.py
+++ addon/globalPlugins/quickDictionary/yandex/settings.py
@@ -15,15 +15,13 @@ import gui
 import wx
 import config
 from . import secret
-from .dictionary import NAME
-from .languages import langs
-from .. import _addonName, _addonSummary
-from ..synthesizers import profiles
+from .dictionary import NAME, SUMMARY
+from ..ui import _addonName, _addonSummary, langs, QDSettingsPanel
 
 
-class QuickDictionarySettingsPanel(gui.SettingsPanel):
+class QuickDictionarySettingsPanel(QDSettingsPanel):
 	"""Add-on settings panel object"""
-	title = _addonSummary
+	title = "{addon}, {service}".format(addon=_addonSummary, service=SUMMARY)
 
 	def __init__(self, parent):
 		"""Initializing the add-on settings panel object"""
@@ -74,42 +72,8 @@ class QuickDictionarySettingsPanel(gui.SettingsPanel):
 		self._useMirrorChk.SetValue(config.conf[_addonName][NAME]['mirror'])
 		sizer.Add(self._useMirrorChk)
 
-		# Translators: A setting in addon settings dialog.
-		self._switchSynthChk = wx.CheckBox(self, label=_("Switch between &voice synthesizers for selected languages"))
-		sizer.Add(self._switchSynthChk)
-
-		# Output of the list of synthesizers for language binding
-		synthSizer = wx.GridSizer(cols=0, vgap=1, hgap=3)
-		self._synthLangsChoice = {}
-		if not next(iter(profiles), None):
-			# Translators: A warning in addon settings dialog.
-			synthWarning = wx.TextCtrl(self, value=_("Please set up voice synthesizers profiles."), size=(200, 30),
-				style=wx.TE_READONLY | wx.TE_MULTILINE | wx.TE_NO_VSCROLL | wx.TE_CENTER | wx.TE_PROCESS_TAB)
-			synthSizer.Add(synthWarning, proportion=wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_CENTER_VERTICAL, flag=wx.EXPAND)
-		else:
-			synthSizer.SetCols(2)
-			synthSizer.SetRows(len(profiles))
-			synthLabel = {}
-			for slot, profile in profiles:
-				synthLabel[slot] = wx.StaticText(self, label="&%d. %s:" % (slot, profile.title), style=wx.ALIGN_RIGHT)
-				self._synthLangsChoice[slot] = wx.Choice(self, choices=[], style=wx.CB_SORT)
-				synthSizer.Add(synthLabel[slot], wx.ALIGN_RIGHT)
-				synthSizer.Add(self._synthLangsChoice[slot], proportion=wx.ALIGN_LEFT)
-		sizer.Add(synthSizer, proportion=wx.ALIGN_CENTER_HORIZONTAL, border=wx.ALL)
-		self._switchSynthChk.Bind(wx.EVT_CHECKBOX, lambda evt, sizer=sizer, synthSizer=synthSizer: sizer.Show(synthSizer, show=evt.IsChecked()) and sizer.Layout())
-		self._switchSynthChk.SetValue(config.conf[_addonName]['switchsynth'])
-		sizer.Show(synthSizer, show=self._switchSynthChk.GetValue())
-
-		# Setting initial values in choices and interaction between choices
-		self._langs = [langs['']] + [l for l in langs.fromList()]
-		self._choices = dict({slot: profile.lang for slot, profile in profiles})
-		for slot, profile in profiles:
-			self.widgetMakerExclude(self._synthLangsChoice[slot], slot)
-			item = self._synthLangsChoice[slot].FindString(langs[profile.lang].name)
-			if item<0:
-				item = self._synthLangsChoice[slot].FindString(langs[''].name)
-			self._synthLangsChoice[slot].Select(item)
-			self._synthLangsChoice[slot].Bind(wx.EVT_CHOICE, lambda evt, sl=slot: self.onSelectSynthLang(evt, sl))
+		# Display a block for associating voice synthesizers with selected languages
+		super(QuickDictionarySettingsPanel, self).makeAssociateSynths(sizer)
 
 		# Field for input access token and link to registration
 		tokenSizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -150,42 +114,13 @@ class QuickDictionarySettingsPanel(gui.SettingsPanel):
 		intoLang = self._intoChoice.FindString(langs[config.conf[_addonName][NAME]['into']].name)
 		self._intoChoice.Select(intoLang if intoLang>=0 else 0)
 
-	def widgetMakerExclude(self, widget, slot: int):
-		"""Creating a widget based on the sequence of Language classes to display it in a wx.Choice object.
-		Exclude from current Choice menu items selected in other Choices.
-		@param widget: widget based on a sequence of Language classes
-		@type widget: wx.Choice
-		@param slot: a number that identifies the current profile of the speech synthesizer
-		@type slot: int
-		"""
-		for lang in self._langs:
-			if lang.code not in [l for s,l in self._choices.items() if l and s!=slot]:
-				widget.Append(lang.name, lang)
-
-	def onSelectSynthLang(self, event, slot: int):
-		"""Fill in the linked Choices and set the initial values.
-		@param event: event indicating the selection of an item in the wx.Choice object
-		@type event: wx.core.PyEventBinder
-		@param slot: a number that identifies the current profile of the speech synthesizer
-		@type slot: int
-		"""
-		choice = self._synthLangsChoice[slot].GetClientData(self._synthLangsChoice[slot].GetSelection())
-		self._choices[slot] = choice.code
-		for sl,prof in profiles:
-			if sl != slot:
-				self._synthLangsChoice[sl].Clear()
-				self.widgetMakerExclude(self._synthLangsChoice[sl], sl)
-				item = self._synthLangsChoice[sl].FindString(langs[self._choices[sl]].name)
-				if item<0:
-					item = self._synthLangsChoice[sl].FindString(langs[''].name)
-				self._synthLangsChoice[sl].Select(item)
-
 	def postInit(self):
 		"""Set system focus to source language selection dropdown list."""
 		self._fromChoice.SetFocus()
 
 	def onSave(self):
 		"""Update Configuration when clicking OK."""
+		super(QuickDictionarySettingsPanel, self).onSave()
 		fromLang = self._fromChoice.GetClientData(self._fromChoice.GetSelection()).code
 		intoLang = self._intoChoice.GetClientData(self._intoChoice.GetSelection()).code
 		config.conf[_addonName][NAME]['from'] = fromLang
@@ -193,10 +128,5 @@ class QuickDictionarySettingsPanel(gui.SettingsPanel):
 		config.conf[_addonName][NAME]['copytoclip'] = self._copyToClipboardChk.GetValue()
 		config.conf[_addonName][NAME]['autoswap'] = self._autoSwapChk.GetValue()
 		config.conf[_addonName][NAME]['mirror'] = self._useMirrorChk.GetValue()
-		config.conf[_addonName]['switchsynth'] = self._switchSynthChk.GetValue()
 		accessToken = self._tokenInput.GetValue()
 		config.conf[_addonName][NAME]['token'] = accessToken if accessToken else secret.APIKEY
-		if config.conf[_addonName]['switchsynth']:
-			for slot, profile in profiles:
-				profiles[slot].lang = self._choices[slot]
-			profiles.save()
